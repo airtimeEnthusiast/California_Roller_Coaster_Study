4b6c7a6731be7a2482421e10c0c22140e2db7344
Austin Wright <9201assumplon9201@gmail.com>
2021-11-11 00:03:45 -0700

    CACoaster Commit With Mode

---
+++ Sources.R
@@ -0,0 +1,19 @@
+*^Ouput HTML output: html_document ^*
+  
+  
+  *https ://rcdb.com/r.htm?ot=2&ex&ol=1499				CA existant coasters according to RCDB*
+    *More info on Big Thunder https://en.wikipedia.org/wiki/Big_Thunder_Mountain_Railroad		*		
+    *https://stg.statcrunch.com/app/index.php?dataid=2188685				*
+      *https://www.youtube.com/watch?v=FfDonRPuYZg				Coastersaurus*
+        *https://www.youtube.com/watch?v=QIG3wVbR6pw				 Canyon Blaster*
+          *https://rcdb.com/r.htm?ot=2&mo=8280				Coast Rider*
+            *https://rcdb.com/4003.htm			Same model as cobra	*
+            *https://sbfrides.com/products/spinning-coaster/				 SBF Visa Spinning coaster*
+            *https://www.youtube.com/watch?v=W8oEo96_QnU				 Dragon Legoland*
+              *https://coasterpedia.net/wiki/Dragon_(Legoland_California)			*	
+              *https://rcdb.com/1478.htm			Same model as Goofy's Sky School	*
+*https://en.wikipedia.org/wiki/The_Grizzly				*
+*https://en.wikipedia.org/wiki/Incredicoaster				*
+*https://en.wikipedia.org/wiki/Matterhorn_Bobsleds				*
+*https://en.wikipedia.org/wiki/Space_Mountain_(Disneyland)				*
+*https://www.ultimaterollercoaster.com/coasters/timber-twister				*
